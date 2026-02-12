9e90356d3246dc33a9d858f2240d78c227381c8c
Manisha Rana <manisharana02005@gmail.com>
2026-02-12 13:10:51 +0530

    Created a basic AI model using google ai studio

---
+++ HowLLMWorks/LLM.js
@@ -0,0 +1,27 @@
+
+
+// 1. create API in google ai studio "https://aistudio.google.com/"
+// API-> AIzaSyBNTC7qQ08HY67GmDL-X02lfuIWdietO9A
+//2. Copy code from the documentation
+//3. you can go to the @google/genai documentation also "https://www.npmjs.com/package/@google/genai"
+//4.  npm i @google/genai
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "what is my name",
+  });
+  console.log(response.text);
+}
+
+await main();
+
+
+//but it will not able to tell my name because it cant store my name, not even the previous question i asked
+
+//but chatgpt keep context of the previous questions, it will send all the qustions to the llm model to keep context, so 
+//it remembers our names if we ask it 
