605cf6a233a81b66610a70fb267eb8369abf2150
Manisha Rana <manisharana02005@gmail.com>
2026-02-12 16:17:06 +0530

    Provided context to my AI model

--- HowLLMWorks/LLM.js
+++ HowLLMWorks/LLM.js
@@ -6,6 +6,26 @@
 //3. you can go to the @google/genai documentation also "https://www.npmjs.com/package/@google/genai"
 //4.  npm i @google/genai
 
+// import { GoogleGenAI } from "@google/genai";
+
+// const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     contents: "what is my name",
+//   });
+//   console.log(response.text);
+// }
+
+// await main();
+
+
+//but it will not able to tell my name because it cant store my name, not even the previous question i asked
+
+//but chatgpt keep context of the previous questions, it will send all the qustions to the llm model to keep context, so 
+//it remembers our names if we ask it 
+
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"});
@@ -13,15 +33,23 @@ const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"})
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
-    contents: "what is my name",
+    contents: [
+      {
+        role: "user",
+        parts: [{ text: "What is my name" }],
+      },
+       {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+      {
+        role: "user",
+        parts: [{ text: "My name is Manisha" }],
+      },
+     
+    ],
   });
   console.log(response.text);
 }
 
 await main();
-
-
-//but it will not able to tell my name because it cant store my name, not even the previous question i asked
-
-//but chatgpt keep context of the previous questions, it will send all the qustions to the llm model to keep context, so 
-//it remembers our names if we ask it 
