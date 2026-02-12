dc096513d5752dc3db54f5cbd7ec8cfcc6a9f0e6
Manisha Rana <manisharana02005@gmail.com>
2026-02-12 20:36:44 +0530

    Maintained all the history of the AI model

--- HowLLMWorks/LLM.js
+++ HowLLMWorks/LLM.js
@@ -5,6 +5,9 @@
 //2. Copy code from the documentation
 //3. you can go to the @google/genai documentation also "https://www.npmjs.com/package/@google/genai"
 //4.  npm i @google/genai
+//5.  npm i readline-sync  => Synchronous Readline for interactively running to have a conversation with the user via a console
+
+
 
 // import { GoogleGenAI } from "@google/genai";
 
@@ -26,30 +29,75 @@
 //but chatgpt keep context of the previous questions, it will send all the qustions to the llm model to keep context, so 
 //it remembers our names if we ask it 
 
+
+//----------------------------------------WITH CONTEXT-------------------------------------------------------------------------------------------
+
+//if we provide context (the chat history) it will remember it
+
+// import { GoogleGenAI } from "@google/genai";
+
+// const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     contents: [
+//       {
+//         role: "user",
+//         parts: [{ text: "What is my name" }],
+//       },
+//        {
+//         role: "model",
+//         parts: [{ text: "Great to meet you. What would you like to know?" }],
+//       },
+//       {
+//         role: "user",
+//         parts: [{ text: "My name is Manisha" }],
+//       },
+     
+//     ],
+//   });
+//   console.log(response.text);
+// }
+
+// await main();
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+// npm i readline-sync  => Synchronous Readline for interactively running to have a conversation with the user via a console
+
+import readlineSync from 'readline-sync';
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyBeJ368RKopqUE04ojd8lm1jfjVWZ0oYJM"});
 
-async function main() {
-  const response = await ai.models.generateContent({
+const History = []
+
+async function Chatting(userProblem) {
+
+  History.push({  //sending the user problem in history array
+    role: 'user',
+    parts:[{text:userProblem}]
+  })
+
+  const response = await ai.models.generateContent({ //it will generate response
     model: "gemini-3-flash-preview",
-    contents: [
-      {
-        role: "user",
-        parts: [{ text: "What is my name" }],
-      },
-       {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-      {
-        role: "user",
-        parts: [{ text: "My name is Manisha" }],
-      },
-     
-    ],
+    contents: History,
   });
-  console.log(response.text);
+
+  History.push({ //storing the response
+    role: 'model',
+    parts:[{text:userProblem}]
+  })
+
+  console.log('\n');
+  console.log(response.text); //printing the response
 }
 
-await main();
+async function main(){
+    const userProblem = readlineSync.question("Ask me anything-->"); //we can take input by using readlineSync
+    await Chatting(userProblem);
+    main(); //main function will be called again and again 
+}
+
+main();
